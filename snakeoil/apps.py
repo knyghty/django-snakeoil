9d8dd6dd7b9e5a9bf7f132c43108fb8061d99c58
Tom Carrick <knyght@gmail.com>
2015-03-24 14:06:54 +0000

    Mark strings for translation

--- snakeoil/apps.py
+++ snakeoil/apps.py
@@ -1,9 +1,10 @@
 from __future__ import unicode_literals
 
 from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
 
 
 class SnakeoilConfig(AppConfig):
     label = 'seo'
     name = 'snakeoil'
-    verbose_name = 'SEO'
+    verbose_name = _('SEO')
--- snakeoil/models.py
+++ snakeoil/models.py
@@ -2,13 +2,16 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
+from django.utils.translation import ugettext_lazy as _
 
 from .utils import get_seo_model
 
 
 class SeoModel(models.Model):
-    head_title = models.CharField(blank=True, max_length=55)
-    meta_description = models.TextField(blank=True, max_length=160)
+    head_title = models.CharField(blank=True, max_length=55,
+                                  verbose_name=_('head title'))
+    meta_description = models.TextField(blank=True, max_length=160,
+                                        verbose_name=_('meta description'))
 
     class Meta:
         abstract = True
@@ -16,7 +19,12 @@ class SeoModel(models.Model):
 
 @python_2_unicode_compatible
 class SeoUrl(get_seo_model()):
-    url = models.CharField(primary_key=True, max_length=255, unique=True)
+    url = models.CharField(primary_key=True, max_length=255, unique=True,
+                           verbose_name=_('URL'))
+
+    class Meta:
+        verbose_name = _('SEO URL')
+        verbose_name_plural = _('SEO URLs')
 
     def __str__(self):
         return self.url
