79bb728dec0968f5cb7bcf597642f82a2f3d972a
Tom Carrick <knyght@knyg.ht>
2016-01-19 18:01:28 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,8 +14,7 @@ setup(
     long_description='A simple Django SEO module that allows attaching titles '
                      'and meta descriptions to objects and URLs',
     description='Simple Django SEO module',
-    install_requires=['django >= 1.7'],
-    requires=['django (>= 1.7)'],
+    install_requires=['django>=1.8'],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
@@ -27,9 +26,8 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Software Development :: Libraries :: Python Modules',
