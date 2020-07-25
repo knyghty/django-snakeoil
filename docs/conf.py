226a936c90b501b3e3a63e5513a6e5858c5712a2
Tom Carrick <tom@carrick.eu>
2020-07-25 13:54:50 +0200

    Docs config

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,5 @@
+import sphinx_rtd_theme  # noqa: F401
+
 # Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
@@ -27,7 +29,7 @@ author = "Tom Carrick"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinxcontrib.spelling"]
+extensions = ["sphinxcontrib.spelling", "sphinx_rtd_theme"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -45,7 +47,7 @@ master_doc = "index"
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
