b0041a511d9dbb422fe2c26bf06d945b6a69b8de
Tom Carrick <tom@carrick.eu>
2020-07-27 16:46:52 +0200

    Allow blank meta tags

---
+++ snakeoil/migrations/0002_auto_20200727_0951.py
@@ -0,0 +1,23 @@
+from django.db import migrations
+
+
+try:
+    from django.db.models import JSONField
+
+except ImportError:
+    from django.contrib.postgres.fields import JSONField
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("snakeoil", "0001_initial"),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name="seopath",
+            name="meta_tags",
+            field=JSONField(blank=True, default=dict, verbose_name="meta tags"),
+        ),
+    ]
